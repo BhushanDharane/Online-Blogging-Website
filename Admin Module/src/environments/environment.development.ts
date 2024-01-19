a2070442c7043fe135ee87f2d882e6c62fff24ea
BhushanDharane <156442041+BhushanDharane@users.noreply.github.com>
2024-01-19 09:27:15 -0500

    Add files via upload

---
+++ Admin Module/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDqwvTd8NUpS-JOfvfz58DIStiuRfMglR4",
+        authDomain: "ang-blog-627ff.firebaseapp.com",
+        projectId: "ang-blog-627ff",
+        storageBucket: "ang-blog-627ff.appspot.com",
+        messagingSenderId: "459333543227",
+        appId: "1:459333543227:web:233832fce40a6379c5431e"
+    }
+
+};
